636be8a5456b377c718521f0ed35a4cb7de2cfa2
Hanna <anna_kon@bigmir.net>
2023-05-04 23:10:55 +0300

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+// const Home = lazy(() => import('../pages/Home.jsx'));
+// const Register = lazy(() => import('../pages/Register.jsx'));
+// const Login = lazy(() => import('../pages/Login.jsx'));
+// const Contacts = lazy(() => import('../pages/Contacts.jsx'));
+
+export const App = () => {
+  return <div>ДІВ</div>;
+};
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { App } from 'components/App';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ToastContainer
+      position="top-right"
+      autoClose={4000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
+    {/* <Provider store={store}> */}
+    {/* <PersistGate loading={null} persistor={persistor}> */}
+    <BrowserRouter basename="/test-task">
+      <App />
+    </BrowserRouter>
+    {/* </PersistGate> */}
+    {/* </Provider> */}
+  </React.StrictMode>
+);
