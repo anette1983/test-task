18fa9167fa68223d7cf7e3f0ac93486cedb14943
Hanna <anna_kon@bigmir.net>
2023-05-04 23:13:49 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { persistor, store } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { persistor, store } from './redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
