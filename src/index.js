676ac16a663d2e4e118985ba99179b197abdc28c
Hanna <anna_kon@bigmir.net>
2023-05-09 01:30:43 +0300

    added bug fixes, deleted unnecessary comments and files

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { persistor, store } from './redux/store';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store';
 import { App } from 'components/App';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
-import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
